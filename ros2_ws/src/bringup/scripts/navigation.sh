bee831b29c908d22c77200d8f28361d76ed59cb4
Mo Wu <mw2335@cornell.edu>
2026-04-27 12:18:41 -0400

    modify navigation.sh

--- ros2_ws/src/bringup/scripts/navigation.sh
+++ ros2_ws/src/bringup/scripts/navigation.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+WORKSPACE=/home/ubuntu/Lost_Found_book/ros2_ws
+
 gnome-terminal \
---tab -e "zsh -c 'source $HOME/.zshrc;sudo systemctl stop start_app_node.service;rviz2 rviz2 -d /home/ubuntu/Lost_Found_book/ros2_ws/src/navigation/rviz/navigation_desktop.rviz'" \
---tab -e "zsh -c 'source $HOME/.zshrc;ros2 launch navigation navigation.launch.py map:=map_01'"
+--tab -e "zsh -c 'source $HOME/.zshrc; source /opt/ros/humble/setup.zsh; sudo systemctl stop start_app_node.service; rviz2 -d ${WORKSPACE}/src/navigation/rviz/navigation_desktop.rviz; exec zsh'" \
+--tab -e "zsh -c 'source $HOME/.zshrc; source /opt/ros/humble/setup.zsh; cd ${WORKSPACE}; source install/navigation/share/navigation/package.zsh; export need_compile=False; export HOST=/; export MASTER=/; ros2 launch ${WORKSPACE}/src/navigation/launch/navigation.launch.py map:=map_01; exec zsh'"
